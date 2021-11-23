5110ca4f7595855d82e47f0d9baf0447d233ca9c
Kierinter <85868981+Kierinter@users.noreply.github.com>
2021-11-23 13:55:56 +0800

    Update search.php

--- 第六次作业/php/search.php
+++ 第六次作业/php/search.php
@@ -1,8 +1,9 @@
-<body>
 <?php
 $chars=$_POST["chars"];
 header("Content-type:index/html;charset=utf-8");
 $database=file(`database.txt`);//以数组导入
 $s=preg_grep(".$chars.",$database);
+ehco '<pre>';
 var_dump($s);
+echo '</pre>';
 ?>

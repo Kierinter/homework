c864c4a90ff7177af9b1f77df34dfbfaaf8e5100
Kierinter <85868981+Kierinter@users.noreply.github.com>
2021-11-27 20:43:17 +0800

    Update insert.php

--- 第七次作业/insert.php
+++ 第七次作业/insert.php
@@ -1,18 +1,23 @@
 <?php
-//
-////$username = $_POST['username'];
-////$password = $_POST['password'];
-////$comment = $_POST['comment'];
-$username ='kierin';
-$password ='123';
-$comment ='test';
-$conn = new mysqli('localhost','root','root','CommentBoard');
-if($username=''||$password=''||$comment='')
-    echo "error:请补足信息";
-    $conn->close();
-    //我觉得这个必填与否应该是前端搞的，因为可以减少数据传输的次数。
 
-$stmt = $conn->prepare("INSERT INTO CommentBoard (username, password, comment) VALUES (?, ?, ?)");
-$stmt->bind_param("sss", $username, $password,$comment);
-$code ="0";
-$message ="insert success";
+$username = $_POST['username'];
+$password = $_POST['password'];
+$comment = $_POST['comment'];
+$conn = new mysqli('localhost','root','root','CommentBoard');
+$sql = "SELECT `username` FROM `commentboard` WHERE `username`=$username ";
+if($conn->multi_query($sql)==true)
+{
+    $stmt = $conn->prepare("INSERT INTO CommentBoard (username, password, comment) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $password,$comment);
+    if($stmt->execute()) {
+        $code = "0";
+        $message = "insert success";
+    }
+    else{
+        $code ="1";
+        $message ="insert fail";
+    }
+}
+else
+    $code="1";
+    $message ="please try an new name";
